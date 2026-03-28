0916d51d47c8e910df3981c9d55414725dec78f9
Harshpreet Kaur <kaurharshpreet714@gmail.com>
2026-03-28 14:31:58 +0530

    mutiple stock prices and historical api integration

--- app/src/main/java/com/example/greenequitysimulator/network/ApiService.java
+++ app/src/main/java/com/example/greenequitysimulator/network/ApiService.java
@@ -3,12 +3,23 @@ package com.example.greenequitysimulator.network;
 import retrofit2.Call;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
-
+import com.example.greenequitysimulator.network.HistoryResponse;
 public interface ApiService {
 
+    // 🔹 API 1: Current Price
     @GET("quote")
     Call<StockResponse> getStockPrice(
             @Query("symbol") String symbol,
+            @Query("token") String token
+    );
+
+    // 🔹 API 2: Historical Data
+    @GET("stock/candle")
+    Call<HistoryResponse> getHistory(
+            @Query("symbol") String symbol,
+            @Query("resolution") String resolution,
+            @Query("from") long from,
+            @Query("to") long to,
             @Query("token") String token
     );
 }
---
+++ app/src/main/java/com/example/greenequitysimulator/network/HistoryResponse.java
@@ -0,0 +1,7 @@
+package com.example.greenequitysimulator.network;
+
+public class HistoryResponse {
+    public String s;
+    public float[] c;
+    public long[] t;
+}
